171cb5294835684933bf4f2805f3733484a582c1
Falko <162439151+hcplayer1988@users.noreply.github.com>
2025-12-01 08:52:07 +0100

    backend finished

--- core/settings.py
+++ core/settings.py
@@ -61,7 +61,7 @@ CSRF_TRUSTED_ORIGINS = [
 
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:5500',
-    'http://localhost:5500', #hier den localhost des frontends eintragen.
+    'http://localhost:5500',
 ]
 
 
